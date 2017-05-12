ffffc6f4536d8e700f07231ff05643394d2d2154
Whispurr <wwllmm777@gmail.com>
2017-05-12 10:39:42 -0700

    William Mao- Contact app instantiation
    Database helper instantion and some xml stuff

--- MyApplication/app/src/main/java/com/example/maow6390/myapplication/DatabaseHelper.java
+++ MyApplication/app/src/main/java/com/example/maow6390/myapplication/DatabaseHelper.java
@@ -13,17 +13,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "contact_table";
     public static final String col_1 = "ID";
     public static final String col_2 = "NAME";
-    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        db.execSQL("DROP TABLE IF EXISTS" + TABLE_NAME);
+        onCreate(db);
     }
 }
