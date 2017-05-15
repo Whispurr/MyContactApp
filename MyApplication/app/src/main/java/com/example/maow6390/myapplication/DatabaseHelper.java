52f61b4c2111ed1c96f70fc8cd1e44b203bf4430
Whispurr <wwllmm777@gmail.com>
2017-05-15 11:13:40 -0700

    William Mao -Added button fuctionality and toasts
    William Mao- Button functionality, toast mesages, start of multiple text
    imputs

--- MyApplication/app/src/main/java/com/example/maow6390/myapplication/DatabaseHelper.java
+++ MyApplication/app/src/main/java/com/example/maow6390/myapplication/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.maow6390.myapplication;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -13,6 +14,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "contact_table";
     public static final String col_1 = "ID";
     public static final String col_2 = "NAME";
+    public static final String col_3 = "AGE";
+    public static final String col_4 = "ADDRESS";
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
@@ -20,7 +23,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL("CREATE TABLE " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, AGE TEXT, ADDRESS TEXT)");
     }
 
     @Override
@@ -28,5 +31,19 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         db.execSQL("DROP TABLE IF EXISTS" + TABLE_NAME);
         onCreate(db);
+    }
+    public boolean insertData(String name, String age, String address) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(col_2,name);
+        contentValues.put(col_3,age);
+        contentValues.put(col_4,address);
+        long result = db.insert(TABLE_NAME,null,contentValues);
+        if (result == -1 ) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 }
--- MyApplication/app/src/main/java/com/example/maow6390/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/maow6390/myapplication/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.maow6390.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 DatabaseHelper myDb;
     EditText editName;
+    EditText editAge;
+    EditText editAddress;
     Button btnAddData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,29 @@ DatabaseHelper myDb;
         setContentView(R.layout.activity_main);
 
         myDb = new DatabaseHelper(this);
+        //add the layout bars
+        editName = (EditText) findViewById(R.id.editText_Name);
+        editAge = (EditText) findViewById(R.id.editText_Age);
+        editAddress = (EditText) findViewById(R.id.editText_address);
+    }
+    public void addData(View v) {
+        boolean isInserted = (myDb.insertData(editName.getText().toString(),editAge.getText().toString(),editAddress.getText().toString()));
+
+        String text = "";
+        if (isInserted == true) {
+            Log.d("MyContact", "Success inserting data");
+            text = "data inserted";
+            Toast toast = Toast.makeText(getApplicationContext(), text,Toast.LENGTH_SHORT);
+            toast.show();
+            //insert toast message here...
+        }
+        else {
+            text = "data failure";
+            Toast toast = Toast.makeText(getApplicationContext(), text,Toast.LENGTH_SHORT);
+            toast.show();
+                Log.d("MyContact", "SFailer inserting data");
+        //insert toast message here...
+    }
+
     }
 }
